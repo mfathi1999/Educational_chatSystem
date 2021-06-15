da7e9dabadbf5a95de54bf7becde157598c29b7f
mohammad <fathim2000astronomy@gmail.com>
2021-06-15 14:12:44 +0430

    bootstrap anf popper installed

--- chatsystem/src/App.vue
+++ chatsystem/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LoginBox />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginBox from './components/LoginBox.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LoginBox
   }
 }
 </script>
---
+++ chatsystem/src/components/LoginBox.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>This is Login Box</h1>
+</template>
